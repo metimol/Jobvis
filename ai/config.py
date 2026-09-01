4dbc116667509d012e4220cd52083aad253eb007
Vladyslav <154080171+metimol@users.noreply.github.com>
2026-09-01 11:12:54 +0200

    Change AI model

--- ai/config.py
+++ ai/config.py
@@ -4,7 +4,7 @@ from langchain.chat_models import init_chat_model
 from const import GOOGLE_API_KEY
 
 model = init_chat_model(
-    model="google_genai:gemma-4-31b-it",
+    model="google_genai:gemma-4-26b-a4b-it",
     api_key=GOOGLE_API_KEY,
     max_tokens=8192,
 )
